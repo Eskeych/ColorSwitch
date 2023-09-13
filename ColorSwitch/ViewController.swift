02b4424a314bc338a7d6e07da760985ca8c77563
Eskeych <mashina631@gmail.com>
2023-09-13 20:14:48 +0300

    form and view. Need a test on the phone

--- ColorSwitch/ViewController.swift
+++ ColorSwitch/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSwitch: UISwitch!
+    @IBOutlet var blueSwitch: UISwitch!
+    @IBOutlet var greenSwitch: UISwitch!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func redSwitchShift(_ sender: Any) {
+        if !redSwitch.isOn{
+            redSlider.value = 0;
+        }
+    }
+    
+    @IBAction func redSliderShift(_ sender: Any) {
+        if redSwitch.isOn{
+            sliderChange()
+        }
+    }
+    
+    @IBAction func greenSliderShift(_ sender: Any) {
+        if greenSwitch.isOn{
+            sliderChange()
+        }
+    }
+    
+    @IBAction func blueSliderShift(_ sender: Any) {
+        if blueSwitch.isOn{
+            sliderChange()
+        }
+    }
+ 
+    func sliderChange(){
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value  / 255),
+            green: CGFloat(greenSlider.value / 255),
+            blue: CGFloat(blueSlider.value / 255), alpha: 1)
+    }
 }
 
